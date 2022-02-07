fee944a5bb5a2e3f87c66750587c32d2f6286c3e
dondrzzy <sibo.donald16@gmail.com>
2022-02-07 12:50:56 +0400

    add exercise 2.5

---
+++ src/books/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+from django.shortcuts import render                     #imported by default
+from django.views.generic import ListView, DetailView   #to display lists and details
+from .models import Book                                #to access Book model             #to access Book model
+
+
+# Create your views here.
+class BookListView(ListView):           #class-based view
+   model = Book                         #specify model
+   template_name = 'books/main.html'    #specify template 
+
+
+class BookDetailView(DetailView):                       #class-based view
+   model = Book                                        #specify model
+   template_name = 'books/detail.html'                 #specify template
