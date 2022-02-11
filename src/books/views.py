6c5086ff40de3e7b161696ddd020f9499514b53e
dondrzzy <sibo.donald16@gmail.com>
2022-02-11 09:19:21 +0400

    add auth

--- src/books/views.py
+++ src/books/views.py
@@ -2,18 +2,18 @@ from django.shortcuts import render
 
 # Create your views here.
 
-
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render                     #imported by default
 from django.views.generic import ListView, DetailView   #to display lists and details
 from .models import Book                                #to access Book model             #to access Book model
 
 
 # Create your views here.
-class BookListView(ListView):           #class-based view
+class BookListView(LoginRequiredMixin, ListView):  #class-based view
    model = Book                         #specify model
    template_name = 'books/main.html'    #specify template 
 
 
-class BookDetailView(DetailView):                       #class-based view
+class BookDetailView(LoginRequiredMixin, DetailView):  #class-based view
    model = Book                                        #specify model
    template_name = 'books/detail.html'                 #specify template
--- src/sales/views.py
+++ src/sales/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
 
 def home(request):
    return render(request, 'sales/home.html')
+
+
+@login_required
+def records(request):
+   #do nothing, simply display page    
+   return render(request, 'sales/records.html')
