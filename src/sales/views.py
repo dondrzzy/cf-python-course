9546edb9a97159ff7c284880363d1e9c606a7261
dondrzzy <sibo.donald16@gmail.com>
2022-02-18 16:47:13 +0300

    add exercise 2.7

--- src/sales/views.py
+++ src/sales/views.py
@@ -1,7 +1,11 @@
+import pandas as pd
+
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+from .forms import SalesSearchForm
+from .models import Sale
+from .utils import get_bookname_from_id, get_chart
 
 
 def home(request):
@@ -10,5 +14,30 @@ def home(request):
 
 @login_required
 def records(request):
-   #do nothing, simply display page    
-   return render(request, 'sales/records.html')
+    #do nothing, simply display page
+    form = SalesSearchForm(request.POST or None)
+    sales_df = None
+    chart = None
+
+    qs = Sale.objects.all()
+
+    #check if the button is clicked
+    if request.method =='POST':
+        book_title = request.POST.get('book_title')
+        chart_type = request.POST.get('chart_type')
+        print (book_title, chart_type)
+
+        qs = Sale.objects.filter(book__name__icontains=book_title)
+
+        if qs:
+            sales_df = pd.DataFrame(qs.values())
+            sales_df['book_id'] = sales_df['book_id'].apply(get_bookname_from_id)
+            chart = get_chart(chart_type, sales_df, labels=sales_df['date_created'].values)
+            sales_df = sales_df.to_html()
+
+    context = {
+        'form': form,
+        'sales_df': sales_df,
+        'chart': chart,
+    }
+    return render(request, 'sales/records.html', context)
